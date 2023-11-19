c0d96d9b0500e1bf9efda8dc0158b9bd67a2486a
G.Karatzas <karatzas.geo@gmail.com>
2023-11-19 22:58:32 +0200

    Change node version

--- mongoDb.js
+++ mongoDb.js
@@ -2,18 +2,24 @@ import mongoose from 'mongoose';
 
 const DB = 'Socket';
 const COLLECTION = 'socket.io-adapter-events';
-mongoose.connect(
-  `mongodb+srv://orionx7:i7HxupA9otqHGDWo@testcluster.t3fpgoc.mongodb.net/${DB}?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+
+try {
+  mongoose.connect(
+    `mongodb+srv://orionx7:i7HxupA9otqHGDWo@testcluster.t3fpgoc.mongodb.net/${DB}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+
+  await mongoCollection.createIndex(
+    { createdAt: 1 },
+    { expireAfterSeconds: 60, background: true }
+  );
+
+  mongoose.connection.on('disconnected', () => {
+    console.log('Disconnected from Db!!!');
+  });
+  mongoose.set('toJSON', { virtuals: true });
+} catch (error) {
+  console.log(error);
+}
 
 export const mongoCollection = mongoose.connection.collection(COLLECTION);
-await mongoCollection.createIndex(
-  { createdAt: 1 },
-  { expireAfterSeconds: 60, background: true }
-);
-
-mongoose.connection.on('disconnected', () => {
-  console.log('Disconnected from Db!!!');
-});
-mongoose.set('toJSON', { virtuals: true });
