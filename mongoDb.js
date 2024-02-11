a70798c9d9b80d0a20d059b9807f50533f593030
G.Karatzas <karatzas.geo@gmail.com>
2024-02-11 11:32:41 +0200

    Add error @ db connection

--- mongoDb.js
+++ mongoDb.js
@@ -7,11 +7,12 @@ const appLogger = debug('frontend');
 try {
   mongoose.set('strictQuery', false); // if true only the fields that are specified in the Schema will be saved
   await mongoose.connect(
-    `mongodb+srv://primitivo:7ZuIFwncwAlka6oX@cluster0.qyvtcbt.mongodb.net/${DB}?retryWrites=true&w=majority`
+    `mongodb+srv://primitivo:ZuIFwncwAlka6oX@cluster0.qyvtcbt.mongodb.net/${DB}?retryWrites=true&w=majority`
   );
   appLogger('🌎 Connection to AdapterDb Succesfull! 🌎');
 } catch (err) {
   appLogger('🌞 Connection to AdapterDb failed', err);
+  process.exit(0);
 }
 
 export const defaultConnection = mongoose.connection;
