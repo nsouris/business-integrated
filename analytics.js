38269a99b0c0091f6b740bff37ba0f44a9799a97
G.Karatzas <karatzas.geo@gmail.com>
2024-02-29 08:40:10 +0200

    Update react app

--- analytics.js
+++ analytics.js
@@ -1,9 +1,7 @@
 import appInsights from 'applicationinsights';
 
 appInsights
-  .setup(
-    'InstrumentationKey=3407c758-105f-433c-b937-e56198ec8bdd;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/'
-  )
+  .setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
   .setAutoDependencyCorrelation(true)
   .setAutoCollectRequests(true)
   .setAutoCollectPerformance(true, true)
